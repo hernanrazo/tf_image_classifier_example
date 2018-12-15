47aa6f7254fadb944ec22bbd88b20308c3b4efb8
Hernan Razo <hernanrazo@gmail.com>
2018-12-14 19:57:28 -0600

    trying to revert

--- app/src/main/java/hernanrazo/tf_image_classifier_example/recognitionScoreView.java
+++ app/src/main/java/hernanrazo/tf_image_classifier_example/recognitionScoreView.java
@@ -18,6 +18,7 @@ public class recognitionScoreView extends View implements resultsView {
     private final float textSizePx;
     private final Paint fgPaint;
     private final Paint bgPaint;
+    //int myColor = context
 
     public recognitionScoreView(final Context context, final AttributeSet set) {
         super(context, set);
